26f2567d37c6353c29430320f2d81620b610aeb1
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-08 12:00:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,8 @@
 def prime?(int)
   if int <= 2
     return FALSE
-  else counter = int - 1
+  else 
+    counter = int - 1
     counter.times do
       print int
       if int % counter == 0 || counter < 1

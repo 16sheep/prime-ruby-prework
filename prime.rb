d2ec87f9ad778f0790069c39129116429570c3a9
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-08 11:48:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,14 +5,11 @@ def prime?(int)
     print "Meaoe"
     return "mo"
   else counter = int - 1
-  counter.times do
-    if int < 0
-      print "Meaoe"
-      break
-    elsif int % counter == 0
-      return FALSE
-      break
-    end
+    counter.times do
+      if int % counter == 0
+        return FALSE
+        break
+      end
   end
   TRUE
 end
